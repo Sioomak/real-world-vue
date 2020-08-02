678ebb7504b86e6d58ed8e4a45526bcb8bd00a14
Siamak Salimi <ssalimi1220@gmail.com>
2020-08-02 00:17:44 -0400

    All Axios calls are now from EventService.js in /services/

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import axios from 'axios'
+import EventService from '@/services/EventService.js'
 
 export default {
   components: {
     EventCard
   },
+  data() {
+    return {
+      events: []
+    }
+  },
   created() {
-    axios
-      .get('http://localhost:3000/events')
+    EventService.getEvents()
       .then(response => {
-        console.log(response.data)
+        this.events = response.data
       })
       .catch(error => {
         console.log("There's an error: " + error.response)
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -2,7 +2,22 @@
   <h1>Showing event #{{ id }}</h1>
 </template>
 <script>
+import EventService from '@/servies/EventSerice.js'
 export default {
-  props: ['id']
+  props: ['id'],
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log("there's an Errror: " + error)
+      })
+  }
 }
 </script>
